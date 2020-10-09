ee6152802585305b98ca77ad7c258850a4c54758
reckonersolutions <solutions.reckoner@gmail.com>
2020-10-09 19:15:51 +0400

    1 file modified

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder} from '@angular/forms';
+import { UserdataService } from '../userdata.service';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  userData;
+  userForm: FormGroup;
+  editUserForm: FormGroup;
+  showEditUserForm = false;
+  editFormId;
+
+  //variable to status of request;
+  userCreated = false;
+  userEdited = false;
+  userDeleted = false;
+  userDeletedId;
+
+  constructor(private userDataService: UserdataService, private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.userDataService.getUserData().subscribe((data)=> {
+      console.log('Data', data);
+      this.userData = data;
+
+    });
+    
+    this.initForm();
+  }
+
+  initForm() {
+    this.userForm = this.fb.group({
+      name:[],
+      username: [],
+      email:[]
+    });
+  }
+
+  createUser(event, userform) {
+    const userData = userform.value;
+
+    this.userDataService.createUserData(userData).subscribe((data) => {
+      console.log('User Data', userData);
+      this.userCreated = true;
+      setTimeout(() => {
+        this.userCreated = false;
+      },2000);
+    });
+  }
+
+  editUser(event, data) {
+    this.showEditUserForm = true;
+    this.edit
   }
 
 }
